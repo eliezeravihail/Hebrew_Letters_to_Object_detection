d9782100808c39a90c1d807a9b4722980f4953ca
eliezer avihail <48094022+eliezeravihail@users.noreply.github.com>
2021-06-03 22:26:55 +0300

    Update createRandomText.jsx

--- createRandomText.jsx
+++ createRandomText.jsx
@@ -4,5 +4,5 @@ var array = "אבגדהוז חטיכל מנס עפצק רשתם ךףץן".split(
 for (var i = 0 ;i<10000;i++){
     res +=  array[Math.floor(Math.random() * array.length)];
     }
-res= res.replace (/(ף)/g, "$1 ").replace (/(ך)/g, "$1 ").replace (/(ץ)/g, "$1 ").replace (/(ן)/g, "$1 ").replace (/(ם)/g, "$1 ")
+res= res.replace(/  +/g," ").replace(/(ף)/g, "$1 ").replace (/(ך)/g, "$1 ").replace (/(ץ)/g, "$1 ").replace (/(ן)/g, "$1 ").replace (/(ם)/g, "$1 ").replace (/(פ )/g, "פ").replace (/(מ )/g, "מ").replace (/(צ )/g, "צ").replace (/(נ )/g, "נ").replace (/(כ)/g, "כ");
 activeDocument.selection[0].parent.texts[0].contents=res;
