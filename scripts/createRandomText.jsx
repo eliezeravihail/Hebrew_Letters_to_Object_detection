042d4b2184762e5fa39a816f009356a037364b37
eliezer avihail <48094022+eliezeravihail@users.noreply.github.com>
2021-06-05 22:55:31 +0300

    Update createRandomText.jsx

--- scripts/createRandomText.jsx
+++ scripts/createRandomText.jsx
@@ -1,8 +1,7 @@
-//select  any text in indesign and run this script:
-var res = ""
-var array = "אבגדהוז חטיכל מנס עפצק רשתם ךףץן".split("");
-for (var i = 0 ;i<10000;i++){
+let res = ""
+const array = "אבגדהוז חטיכל מנס עפצק רשתם ךףץן".split("");
+for (let i = 0 ;i<50000;i++){
     res +=  array[Math.floor(Math.random() * array.length)];
     }
 res= res.replace(/  +/g," ").replace(/(ף)/g, "$1 ").replace (/(ך)/g, "$1 ").replace (/(ץ)/g, "$1 ").replace (/(ן)/g, "$1 ").replace (/(ם)/g, "$1 ").replace (/(פ )/g, "פ").replace (/(מ )/g, "מ").replace (/(צ )/g, "צ").replace (/(נ )/g, "נ").replace (/(כ)/g, "כ");
-activeDocument.selection[0].parent.texts[0].contents=res;
+console.log(res);
